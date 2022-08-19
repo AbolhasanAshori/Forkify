45175b15679694b438ea9ca77138b691414440f9
Abolhasan Ashori <abolhasan.ashori@gmail.com>
2022-08-19 12:19:25 +0430

    Initialize Git

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "d3ddb6cb-8cc2-4ead-b89e-4d768e642733";
+export const MODAL_CLOSE_SEC = 2.5;
